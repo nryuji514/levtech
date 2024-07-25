bc5ffd1f8807d4ffaf067d016641d20b70a6a52d
nryuji514 <e1922048@oit.ac.jp>
2024-07-26 02:03:24 +0900

    ブログ投稿詳細の表示

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
-<html lang="ja">
+<html lang="{{ str_replace('_','-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Blog</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -11,11 +12,13 @@
         <div class='posts'>
             @foreach($posts as $post)
                 <div class='post'>
-                    <h2 class='title'>{{ $post->title }}</h2>
+                    <a href = "/posts/{{ $post->id }}"><h2 class="title">{{$post->title}}</h2></a>
                     <p class='body'>{{ $post->body }}</p>
                 </div>
             @endforeach
         </div>
-        <div class='paginate'>{{ $posts->links()}}</div>
+        <div class='paginate'>
+            {{ $posts->links() }}
+        </div>
     </body>
 </html>
