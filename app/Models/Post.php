9a1892e33a32c0ab0df86cf6538cde3ed97d202b
nryuji514 <e1922048@oit.ac.jp>
2024-07-27 13:47:25 +0900

    ブログ投稿の作成

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,9 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+    
+    protected $fillable= [
+        'title',
+        'body',
+        ];
 
     public function getPaginateByLimit(int $limit_count = 10)
     {
-        return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+        return $this->orderby('updated_at', 'DESC')->paginate($limit_count);
     }
 }
