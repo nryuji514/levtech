a4f037d90e777268129821eb4287203d63a7f7fb
nryuji514 <e1922048@oit.ac.jp>
2024-07-28 18:09:50 +0900

    ブログ投稿の削除

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,6 +6,7 @@
         <title>Blog</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    
     </head>
     <body class="antialiased">
         <h1>Blog Name</h1>
@@ -15,11 +16,23 @@
                 <div class='post'>
                     <a href = "/posts/{{ $post->id }}"><h2 class="title">{{$post->title}}</h2></a>
                     <p class='body'>{{ $post->body }}</p>
+                    <form action="/posts/ {{ $post->id }}" id="form_{{ $post->id }}" method="post">
+                        @csrf
+                        @method('DELETE')
+                        <button type="button" onclick="deletePost({{ $post->id }})">delete</button>
+                    </form>
                 </div>
             @endforeach
         </div>
-        <div class='paginate'>
-            {{ $posts->links() }}
-        </div>
+        <div class='paginate'>{{ $posts->links() }}</div>
+        <script>
+            function deletePost(id) {
+                'use strict'
+                
+               if (confirm('削除すると復元できません。\n本当によろしいですか？')) {
+                   document.getElementById(`form_${id}`).submit();
+               } 
+            }
+        </script>
     </body>
 </html>
